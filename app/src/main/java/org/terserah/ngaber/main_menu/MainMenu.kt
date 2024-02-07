b81ace7d583a3b316176b50d9a1d096700983dc7
MFK <fakhrikurniawan643@gmail.com>
2024-02-07 14:43:28 +0700

    Inbox, Activity, Recommendation

--- app/src/main/java/org/terserah/ngaber/main_menu/MainMenu.kt
+++ app/src/main/java/org/terserah/ngaber/main_menu/MainMenu.kt
@@ -1,46 +1,35 @@
-package org.terserah.ngaber.main_menu
+package org.terserah.ngaber
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.google.firebase.Firebase
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.auth
-import org.terserah.ngaber.AccountMenuFragment
-import org.terserah.ngaber.ActivityMenuFragment
-import org.terserah.ngaber.ChatMenuFragment
-import org.terserah.ngaber.R
 
 class MainMenu : AppCompatActivity() {
     private lateinit var auth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        auth = Firebase.auth
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main_menu)
-        setCurrentFragment(MainMenuFragment())
-
-        auth = Firebase.auth
 
         val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
 
         bottomNavigationView.setOnNavigationItemSelectedListener() {
             when (it.itemId) {
-                R.id.home ->  setCurrentFragment(MainMenuFragment())
-                R.id.chat -> setCurrentFragment(ChatMenuFragment())
-                R.id.activity -> setCurrentFragment(ActivityMenuFragment())
-                R.id.account -> setCurrentFragment(AccountMenuFragment())
+                R.id.home -> startActivity(Intent(this, MainMenu::class.java))
+                R.id.chat -> startActivity(Intent(this, Inbox::class.java))
+                R.id.activity -> startActivity(Intent(this, Activity::class.java))
+                R.id.account -> startActivity(Intent(this, Acccount_Detail::class.java))
+
             }
             true
         }
 
     }
-
-    private fun setCurrentFragment(fragment: Fragment)=
-        supportFragmentManager.beginTransaction().apply {
-            replace(R.id.flFragment,fragment)
-            commit()
-        }
 }
 
 
