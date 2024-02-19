081b81d6266c6ba9a644bfb49b95a30bd2635c00
MFK <fakhrikurniawan643@gmail.com>
2024-02-19 09:59:10 +0700

    security, personal information

---
+++ app/src/main/java/org/terserah/ngaber/PersonalInformation.kt
@@ -0,0 +1,11 @@
+package org.terserah.ngaber
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class PersonalInformation : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_personal_information)
+    }
+}
